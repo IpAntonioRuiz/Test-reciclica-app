49f6d58499b968a05bb451f70dedd57d2a11b803
IpAntonioRuiz <josea290@hotmail.com>
2025-02-27 13:52:12 -0600

    Pagina de login en proceso

--- reciclica-app/src/app/app-routing.module.ts
+++ reciclica-app/src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'folder/inbox', pathMatch: 'full'},
+  {path: 'folder/:id',loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)},
+  {path: 'loader', loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)},
   {
-    path: '',
-    redirectTo: 'folder/inbox',
-    pathMatch: 'full'
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 
